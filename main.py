739f76a067f8194eb529b4b199b973dbb44490ea
Dorus van de Spijker <dorus.van.de.spijker@prodrive-technologies.com>
2020-08-19 01:58:21 +0200

    - main: Do not import report module.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import render_template
 import connexion
-import report
 
 # Create the application instance
 app = connexion.App(__name__, specification_dir='./config')
@@ -20,6 +19,4 @@ def home():
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
-    SetOutputChannels({'data': ['uplink.data'], 'meta': ['uplink.meta'],
-        'url': ['downlink.url']})
     app.run(host='0.0.0.0', port=5000, debug=True)
